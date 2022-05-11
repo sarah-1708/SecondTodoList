7661ac3a58ea99c8bdc398ac8f072a9c02ce8a27
Sarah Rambaud <52890781+sarah-1708@users.noreply.github.com>
2022-05-11 08:45:22 +0200

    Add files via upload

---
+++ secondeTodolist/src/pages/Auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page style="margin-top:-220px">
+    <div class="text-subtitle1 text-center">
+      Afin d'accéder à votre Todolist,<br />merci de bien vouloir vous
+      authentifier.
+    </div>
+
+    <q-btn class="center " color="secondary" label="M'authentifier" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "AuthPage",
+};
+</script>
